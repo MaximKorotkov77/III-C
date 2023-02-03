d6bb0b6dc89341ba9e597c593776c5c558dabefd
Maksim <mkorotkov77@gmail.com>
2023-02-03 15:07:22 +0300

    add Lection02

---
+++ Lection02/10/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] array = {1,2,3,4,5,6,7,8,4,9};
+int n = array.Length;
+int find = 4;
+int i = 0;
+
+while(i<n)
+{if (array[i] == find)
+{Console.Write($"Индекс числа {find} равен {i}");
+return;
+}
+    i++;
+}
