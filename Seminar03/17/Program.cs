0887f3764bdbcd52dd6763a4a70df1e77f8ed077
Maksim <mkorotkov77@gmail.com>
2023-02-06 15:10:01 +0300

    add 17

---
+++ Seminar03/17/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите x: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y: ");
+int y = int.Parse(Console.ReadLine());
+if (x>0 && y>0)
+Console.WriteLine("Плоскость I");
+if (x<0 && y>0)
+Console.WriteLine("Плоскость II");
+if (x<0 && y<0)
+Console.WriteLine("Плоскость III");
+if (x>0 && y<0)
+Console.WriteLine("Плоскость IV");
+if (x==0 && y==0)
+Console.WriteLine("Плоскость 0");
