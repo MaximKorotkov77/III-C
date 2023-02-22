667d6f1f8a1916979b3f87ddad00bf349fcf2d7d
Maksim <mkorotkov77@gmail.com>
2023-02-22 18:13:14 +0300

    HW08

---
+++ HW/59/Program.cs
@@ -0,0 +1,120 @@
+﻿// 59
+// Из двумерного массива целых чисел удалить строку и столбец,
+// на пересечении которых расположен первый найденный наименьший элемент.
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int m = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int n = int.Parse(ReadLine());
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(10);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int Findmin_i (int [,] inArray)
+{ int min = inArray[0,0];
+    int min_i = 0;
+    int min_j = 0;
+    for (int i =0; i<inArray.GetLength(0);i++)
+    {
+        for (int j =0; j<inArray.GetLength(1);j++)
+        {
+            if (min>inArray[i,j])
+            {
+                 min = inArray[i,j];
+                 min_i = i;
+                
+            }
+           
+        }
+    }
+    return min_i;
+    
+}
+
+int Findmin_j (int [,] inArray)
+{ int min = inArray[0,0];
+    int min_i = 0;
+    int min_j = 0;
+    for (int i =0; i<inArray.GetLength(0);i++)
+    {
+        for (int j =0; j<inArray.GetLength(1);j++)
+        {
+            if (min>inArray[i,j])
+            {
+                 min = inArray[i,j];
+               
+                 min_j = j;
+            }
+           
+        }
+    }
+    
+    return min_j;
+}
+
+int[,] ChangeArray(int[,] inArray, int min_i, int min_j)
+{
+    int[,] result = new int[inArray.GetLength(0)-1, inArray.GetLength(1)-1];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+    
+        for (int j = 0; j < result.GetLength(1); j++)
+         {  
+            if (i >= min_i) 
+            {
+                 result[i, j] = inArray[i+1,j];
+            }
+
+            else if (j >= min_j) 
+            {
+                result[i, j] = inArray[i,j+1];
+             }
+            
+            else
+            {
+            result[i, j] = inArray[i,j];
+            }
+         }
+        
+    }
+    return result;
+}
+
+int [,] Array= GetArray(m, n);
+PrintArray(Array);
+WriteLine();
+int min_i = Findmin_i(Array);
+int min_j = Findmin_j(Array);
+WriteLine($"{min_i} {min_j}");
+WriteLine();
+ChangeArray(Array, min_i, min_j);
+PrintArray(ChangeArray(Array, min_i, min_j));
+
---
+++ HW/Задача 5/Program.cs
@@ -0,0 +1,122 @@
+﻿// Задача 5. Заполните спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+// using System;
+// using static System.Console;
+
+// Clear();
+
+// int [,] GetArray()
+// {
+//     int[,] result = new int[4, 4];
+//     int el= 0;
+    
+//     for (int i = 0; i < 1; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {   el=el+1;
+//             result[i, j] = el;
+//         }
+//     break;
+//     }
+    
+//         for (int j = 3; j < 4; j++)
+//     {
+//         for (int i = 1; i < 4; i++)
+//         {   el=el+1;
+//             result[i, j] = el;
+//         }
+//          break;
+//     }
+
+//     for (int i = 3; i < 4; i++)
+//     {
+//         for (int j = 2; j > -1; j--)
+//         {   el=el+1;
+//             result[i, j] = el;
+//         }
+//          break;
+//     }
+
+// //  for (int j = 0; j >1; j++)
+// //     {
+// //         for (int i = 2; i > 0; i--)
+// //         {   el=el+1;
+// //             result[i, j] = el;
+// //         }
+// //     }
+
+
+//     return result;
+// }
+
+
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Write($"{inArray[i, j]} ");
+//         }
+//         WriteLine();
+//     }
+// }
+// PrintArray (GetArray());
+
+Console.Clear();
+
+
+
+int [,] FillImage(int [,] Array, int row, int col)
+{
+    if (Array[row,col] ==0)
+    {  int el =0;
+        Array[row,col] =el+1;
+        FillImage(Array, row -1, col);
+        FillImage(Array, row, col-1);
+        FillImage(Array, row +1, col);
+        FillImage(Array, row , col+1);
+    }
+    return Array;
+}
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = 0;
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+
+int[,] Array = GetArray(4, 4);
+FillImage(Array, 2, 2);
+PrintArray(Array);
