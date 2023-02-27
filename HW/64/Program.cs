bf77e4777129bfbc597aa76ff11b3c87009d35c2
Maksim <mkorotkov77@gmail.com>
2023-02-27 17:15:02 +0300

    add 68

---
+++ HW/64/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите n");
+int n = int.Parse(ReadLine());
+int g = 1;
+
+string PrintNumbers(int g, int n)
+{
+    if (g == n)
+    {
+        WriteLine($"{g}");
+        return g.ToString();
+    }
+    string s = PrintNumbers(g + 1, n) + " " + g.ToString();
+    WriteLine(s);
+    return s;
+}
+
+PrintNumbers(g, n);
+
---
+++ HW/66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите n");
+int n = int.Parse(ReadLine());
+WriteLine("Введите m");
+int m = int.Parse(ReadLine());
+
+int FindSum(int m, int n)
+{
+    if (m == n)
+        return m;
+    return (m + FindSum(m + 1, n));
+}
+
+Write($"{FindSum(m, n)}");
